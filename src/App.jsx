f05e7f9b17d3e9a3ddfb085005907fd06933e50c
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-27 14:52:46 -0400

    Agregando el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 import "./App.css";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <form className="search-form">
+        <label htmlFor="search" className="search-form__label">
+          Search movie
+          <input
+            type="text"
+            id="search"
+            placeholder="Avengers, Mario bros, Pokemon..."
+            value={""}
+            onChange={() => console.log("HOLA")}
+            autoComplete="off"
+          />
+        </label>
+        <input
+          type="button"
+          value="Search Movie"
+          className="search-form__button"
+        />
+      </form>
+    </>
+  );
+}
 
 export default App;
