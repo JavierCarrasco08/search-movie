0b5cbcf7a22529194c39db8a40f4a90436105d8d
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-28 18:16:26 -0400

    Estructura de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useSearchMovie } from "./hooks/useSearchMovie";
+import ContainerMovie from "./components/container-movies/ContainerMovie";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState("");
+  const [value, setValue, data] = useSearchMovie();
   return (
     <>
-      <form className="search-form">
+      <form
+        className="search-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setValue(value);
+        }}>
         <label htmlFor="search" className="search-form__label">
           Search movie
           <input
@@ -13,16 +19,19 @@ function App() {
             id="search"
             placeholder="Avengers, Mario bros, Pokemon..."
             value={value}
-            onChange={(e) => setValue(e.target.value)}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
             autoComplete="off"
           />
         </label>
         <input
-          type="button"
+          type="submit"
           value="Search Movie"
           className="search-form__button"
         />
       </form>
+      <ContainerMovie movies={data} />
     </>
   );
 }
